b9b51669d4ebfc5994198b3f8d22e63cae414d61
Diana <5275194+DianaNites@users.noreply.github.com>
2021-05-09 17:54:53 -0400

    ugh

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,7 +1,18 @@
-#![allow(unused_imports, unused_variables)]
+#![allow(unused_imports, unused_variables, dead_code)]
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
 
+/// The UEFI Entry point
+///
+/// A function with this attribute must appear ONLY ONCE in the entire
+/// dependency tree.
 #[proc_macro_attribute]
-pub fn entry(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn entry(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ItemFn);
     todo!()
+}
+
+fn entry_impl() {
+    //
 }
