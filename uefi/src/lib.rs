ce6203899722fe5099e5169299c76e8e5f38534d
Diana <5275194+DianaNites@users.noreply.github.com>
2022-11-17 17:08:38 -0800

    wip

--- uefi/src/lib.rs
+++ uefi/src/lib.rs
@@ -1,2 +1,19 @@
+#![allow(unused_imports, unused_variables)]
 #![no_std]
-#![feature(abi_efiapi, asm)]
+#![feature(abi_efiapi)]
+use core::{ffi::c_void, sync::atomic::Ordering};
+
+#[repr(transparent)]
+pub struct EfiHandle(*mut c_void);
+
+pub type EfiStatus = usize;
+pub type SystemTable = c_void;
+
+#[no_mangle]
+pub extern "efiapi" fn efi_main(image: EfiHandle, system_table: *mut SystemTable) -> EfiStatus {
+    extern "Rust" {
+        fn main() -> EfiStatus;
+    }
+    // Safety: Must exist or won't link.
+    unsafe { main() }
+}
