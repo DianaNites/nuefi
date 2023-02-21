a76353d07b72b7334375e0eb6a945ccc0338533d
Diana <5275194+DianaNites@users.noreply.github.com>
2023-02-21 10:15:11 -0800

    wip mocking

--- uefi/src/table/raw.rs
+++ uefi/src/table/raw.rs
@@ -1,5 +1,8 @@
 //! Raw UEFI data types
-use core::mem::size_of;
+use core::{
+    mem::size_of,
+    ptr::{null_mut, NonNull},
+};
 
 use crate::{
     error::{EfiStatus, Result},
@@ -25,11 +28,15 @@ pub static CRC: crc::Crc<u32> = crc::Crc::<u32>::new(&crc::CRC_32_ISO_HDLC);
 pub struct Revision(u32);
 
 impl Revision {
-    pub fn major(self) -> u32 {
+    pub const fn new(major: u16, minor: u16) -> Self {
+        Revision(((major as u32) << 16) | minor as u32)
+    }
+
+    pub const fn major(self) -> u32 {
         self.0 >> 16
     }
 
-    pub fn minor(self) -> u32 {
+    pub const fn minor(self) -> u32 {
         self.0 as u16 as u32
     }
 }
@@ -61,6 +68,7 @@ impl Header {
     ///
     /// - Must be called with a valid pointed to a UEFI table
     unsafe fn validate(table: *mut Self, sig: u64) -> Result<()> {
+        assert!(!table.is_null(), "Table Header ({sig}) was null");
         let header = &*table;
         let expected = header.crc32;
         let len = header.size;
@@ -74,7 +82,9 @@ impl Header {
         // Calculate the remaining table, header digested above.
         let bytes = core::slice::from_raw_parts(
             table.cast::<u8>().add(size_of::<Header>()),
-            len as usize - size_of::<Header>(),
+            (len as usize)
+                .checked_sub(size_of::<Header>())
+                .ok_or(EfiStatus::BUFFER_TOO_SMALL)?,
         );
         digest.update(bytes);
         if expected != digest.finalize() {
@@ -160,11 +170,66 @@ impl RawSystemTable {
             header.boot_services as *mut Header,
             RawBootServices::SIGNATURE,
         )?;
+        loop {}
         Header::validate(
             header.runtime_services as *mut Header,
             RawRuntimeServices::SIGNATURE,
         )?;
         Ok(())
+    }
+
+    fn to_bytes(&self) -> &[u8] {
+        // Safety: `self` is valid by definition
+        // Lifetime is bound to self
+        let bytes = unsafe {
+            core::slice::from_raw_parts(self as *const Self as *const u8, size_of::<Self>())
+        };
+        bytes
+    }
+
+    /// Mock instance of [`RawSystemTable`]
+    #[doc(hidden)]
+    #[allow(unreachable_code, unused_mut)]
+    pub unsafe fn mock() -> Self {
+        const MOCK_VENDOR: &[u8] = b"Mock Vendor";
+        let mut mock_vendor = [0u8; MOCK_VENDOR.len()];
+        // let len = MOCK_VENDOR.chars().fold(0, |a, e| a + e.len_utf16());
+        // firmware_vendor.encode_utf16();
+
+        const MOCK_VENDOR_16: *const u16 = null_mut();
+        const MOCK_HEADER: Header = Header {
+            signature: RawSystemTable::SIGNATURE,
+            revision: Revision::new(2, 70),
+            size: size_of::<RawSystemTable>() as u32,
+            crc32: 0,
+            reserved: 0,
+        };
+        const MOCK_SYSTEM: RawSystemTable = RawSystemTable {
+            header: MOCK_HEADER,
+            firmware_vendor: MOCK_VENDOR_16,
+            firmware_revision: 69420,
+            console_in_handle: EfiHandle(null_mut()),
+            con_in: null_mut(),
+            console_out_handle: EfiHandle(null_mut()),
+            con_out: null_mut(),
+            standard_error_handle: EfiHandle(null_mut()),
+            std_err: null_mut(),
+            runtime_services: null_mut(),
+            boot_services: null_mut(),
+            number_of_table_entries: 0,
+            configuration_table: null_mut(),
+        };
+
+        const MOCK_BOOT: RawBootServices = RawBootServices::mock();
+
+        let mut s = MOCK_SYSTEM;
+
+        s.header.crc32 = {
+            let mut digest = CRC.digest();
+            digest.update(s.to_bytes());
+            digest.finalize()
+        };
+        s
     }
 }
 
@@ -306,6 +371,69 @@ pub struct RawBootServices {
 
 impl RawBootServices {
     const SIGNATURE: u64 = 0x56524553544f4f42;
+
+    const fn mock() -> Self {
+        const MOCK_HEADER: Header = Header {
+            signature: RawBootServices::SIGNATURE,
+            revision: Revision::new(2, 70),
+            size: size_of::<RawBootServices>() as u32,
+            crc32: 0,
+            reserved: 0,
+        };
+        let b = [0u8; size_of::<Self>()];
+        // Safety:
+        let mut t: RawBootServices = unsafe { core::mem::transmute::<_, _>(b) };
+        t.header = MOCK_HEADER;
+        return t;
+        #[cfg(no)]
+        RawBootServices {
+            header: MOCK_HEADER,
+            raise_tpl: null_mut(),
+            restore_tpl: null_mut(),
+            allocate_pages: null_mut(),
+            free_pages: null_mut(),
+            get_memory_map: null_mut(),
+            allocate_pool: null_mut(),
+            free_pool: null_mut(),
+            create_event: null_mut(),
+            set_timer: null_mut(),
+            wait_for_event: null_mut(),
+            signal_event: null_mut(),
+            close_event: null_mut(),
+            check_event: null_mut(),
+            install_protocol_interface: null_mut(),
+            reinstall_protocol_interface: null_mut(),
+            uninstall_protocol_interface: null_mut(),
+            handle_protocol: null_mut(),
+            reserved: null_mut(),
+            register_protocol_notify: null_mut(),
+            locate_handle: null_mut(),
+            locate_device_path: null_mut(),
+            install_configuration_table: null_mut(),
+            load_image: null_mut(),
+            start_image: null_mut(),
+            exit: null_mut(),
+            unload_image: null_mut(),
+            exit_boot_services: null_mut(),
+            get_next_monotonic_count: null_mut(),
+            stall: null_mut(),
+            set_watchdog_timer: null_mut(),
+            connect_controller: null_mut(),
+            disconnect_controller: null_mut(),
+            open_protocol: null_mut(),
+            close_protocol: null_mut(),
+            open_protocol_information: null_mut(),
+            protocols_per_handle: null_mut(),
+            locate_handle_buffer: null_mut(),
+            locate_protocol: null_mut(),
+            install_multiple_protocol_interfaces: null_mut(),
+            uninstall_multiple_protocol_interfaces: null_mut(),
+            calculate_crc32: null_mut(),
+            copy_mem: null_mut(),
+            set_mem: null_mut(),
+            create_event_ex: null_mut(),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -317,4 +445,16 @@ pub struct RawRuntimeServices {
 
 impl RawRuntimeServices {
     const SIGNATURE: u64 = 0x56524553544e5552;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn revision() {
+        let rev = Revision::new(2, 70);
+        assert_eq!(rev.major(), 2);
+        assert_eq!(rev.minor(), 70);
+    }
 }
