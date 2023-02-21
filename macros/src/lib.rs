69eccc26de48897b4e4e38bda428fa94a534b97f
Diana <5275194+DianaNites@users.noreply.github.com>
2023-02-20 23:42:34 -0800

    wip macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,17 +1,41 @@
 #![allow(unused_imports, unused_variables, dead_code)]
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, spanned::Spanned, AttributeArgs, Error, ItemFn};
 
 /// The UEFI Entry point
 ///
 /// A function with this attribute must appear ONLY ONCE in the entire
 /// dependency tree.
 #[proc_macro_attribute]
-pub fn entry(_attr: TokenStream, input: TokenStream) -> TokenStream {
-    // https://github.com/dtolnay/proc-macro-workshop
+pub fn entry(args: TokenStream, input: TokenStream) -> TokenStream {
+    let args = parse_macro_input!(args as AttributeArgs);
     let input = parse_macro_input!(input as ItemFn);
-    todo!()
+
+    let sig = input.sig;
+    let attrs = input.attrs;
+    if !attrs.is_empty() {
+        // panic!("Had {} attributes and expected zero", input.attrs.len());
+    }
+    let params = &sig.inputs;
+    if params.is_empty() {
+        let span = sig.span();
+        let err = Error::new(span, "Missing `handle` and `table` parameters");
+        return TokenStream::from(err.into_compile_error());
+    } else if params.len() != 2 {
+        // let span = params.span();
+        // let err = Error::new(span, "");
+        // return TokenStream::from(err.into_compile_error());
+        // return TokenStream::from(quote! {
+        //     compile_error!("");
+        // });
+    }
+
+    let expanded = quote! {
+        // ...
+    };
+
+    TokenStream::from(expanded)
 }
 
 fn entry_impl() {
--- uefi/src/lib.rs
+++ uefi/src/lib.rs
@@ -14,6 +14,7 @@ use core::{
 
 use error::EfiStatus;
 use log::{error, info};
+pub use macros::entry;
 use table::{raw::RawSystemTable, Boot};
 
 pub use crate::table::SystemTable;
