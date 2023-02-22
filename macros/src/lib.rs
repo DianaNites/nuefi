497d35c956426fd704c46ffa46ea6c3e358056f8
Diana <5275194+DianaNites@users.noreply.github.com>
2023-02-22 00:41:05 -0800

    Macro documentation

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -8,8 +8,49 @@ mod proto;
 
 /// The UEFI Entry point
 ///
-/// A function with this attribute must appear ONLY ONCE in the entire
-/// dependency tree.
+/// A function with this attribute must appear once in the entire
+/// dependency tree or link errors will result.
+///
+/// # Options
+///
+/// - `crate = "name"`.
+///     - Changes the root crate used to reference types.
+///     Here you can see we changed `uefi` to `nuefi`, but the macro
+///     would still use `uefi` and fail to compile.
+///     This option solves that problem.
+/// - `log`
+///     - This enables some [`log`][log] statements to be inserted
+///     by the library `efi_main` handler.
+/// - `panic`
+///     - Whether to generate a `panic_impl` or leave it up to you
+/// - `alloc`
+///     - Whether to generate a `alloc_error_handler` or leave it up to you.
+///     This requires [`#![feature(alloc_error_handler)]`][alloc_err].
+/// - `delay(N)`
+///     - Enables a delay of `N` seconds before returning to firmware.
+///     If this is not specified, there is no delay.
+///
+/// # Example
+///
+/// Showing how to use the attribute and some basic options
+///
+/// ```rust
+/// # use nuefi::entry;
+/// # use nuefi::EfiHandle;
+/// # use nuefi::SystemTable;
+/// # use nuefi::table::Boot;
+/// # use nuefi::error::Result;
+/// // Or through the `package` key in `Cargo.toml`!
+/// use uefi as nuefi;
+///
+/// #[entry(crate = "nuefi", delay(69))]
+/// fn e_main(handle: EfiHandle, table: SystemTable<Boot>) -> Result<()> {
+///     Ok(())
+/// }
+/// ```
+///
+/// [log]: <https://crates.io/crates/log>
+/// [alloc_err]: <https://doc.rust-lang.org/nightly/unstable-book/language-features/alloc-error-handler.html>
 #[proc_macro_attribute]
 pub fn entry(args: TokenStream, input: TokenStream) -> TokenStream {
     entry::entry(args, input)
@@ -31,7 +72,7 @@ pub fn entry(args: TokenStream, input: TokenStream) -> TokenStream {
 /// # use uefi::interface;
 /// # use uefi::Protocol;
 /// # pub struct RawMyProtocol;
-///
+/// #
 /// interface!(
 ///     #[Protocol("A46423E3-4617-49F1-B9FF-D1BFA9115839")]
 ///     MyProtocol(RawMyProtocol)
@@ -42,6 +83,8 @@ pub fn entry(args: TokenStream, input: TokenStream) -> TokenStream {
 ///
 /// The GUID MUST be valid for the type signature you provide,
 /// otherwise unsafe/undefined type mismatch and confusion will result.
+///
+/// This macro is only intended to be used by internal developers.
 #[proc_macro_attribute]
 #[allow(non_snake_case)]
 pub fn Protocol(args: TokenStream, input: TokenStream) -> TokenStream {
