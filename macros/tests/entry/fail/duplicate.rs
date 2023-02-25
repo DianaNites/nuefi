8525d4c7d6d5810e8b31342ba034ba931a7bbfbe
Diana <5275194+DianaNites@users.noreply.github.com>
2023-02-24 21:56:04 -0800

    test crate duplicate

--- macros/tests/entry/fail/duplicate.rs
+++ macros/tests/entry/fail/duplicate.rs
@@ -1,7 +1,7 @@
 //! Tests whether duplicate argument failures are nice
 use nuefi::{entry, error::Result, table::Boot, EfiHandle, SystemTable};
 
-#[entry(exit_prompt, log, exit_prompt, log, delay(69), delay(420))]
+#[entry(exit_prompt, log, exit_prompt, log, delay(69), delay(420), crate = "nuefi", crate = "nuefi")]
 fn e_main(_handle: EfiHandle, _table: SystemTable<Boot>) -> Result<()> {
     Ok(())
 }
