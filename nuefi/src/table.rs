12b1a8ff94e74cca5165b737b92b79bf217ea599
Diana <5275194+DianaNites@users.noreply.github.com>
2023-02-28 22:57:13 -0800

    todo

--- nuefi/src/table.rs
+++ nuefi/src/table.rs
@@ -283,6 +283,8 @@ impl<'table> BootServices<'table> {
     /// Query `handle` to determine if it supports `Protocol`
     ///
     /// If no protocol is found, [`Ok(None)`] is returned.
+    // TODO: Is this safe/sound to call with the same protocol twice?
+    // Same reasons as `open_protocol`
     pub fn handle_protocol<'boot, Protocol: proto::Protocol<'boot>>(
         &'boot self,
         handle: EfiHandle,
