6495c0ef6b409f9c8202d7eeab6b47c7dca3274c
Diana <5275194+DianaNites@users.noreply.github.com>
2023-03-01 06:58:06 -0800

    wip

--- nuefi/src/proto/device_path/raw.rs
+++ nuefi/src/proto/device_path/raw.rs
@@ -1,3 +1,47 @@
+/// [`RawDevicePath`] types
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(transparent)]
+pub struct RawDevicePathType(u8);
+
+impl RawDevicePathType {
+    /// Represents a device connected to the system
+    pub const HARDWARE: Self = Self(0x01);
+
+    /// Represents ACPI Plug and Play hardware?
+    pub const ACPI: Self = Self(0x02);
+
+    /// Represents the connection to a device on another system,
+    /// such as a IP address or SCSI ID.
+    pub const MESSAGING: Self = Self(0x03);
+
+    /// Represents the portion of an entity that is being abstracted,
+    /// such as a file path on a storage device.
+    pub const MEDIA: Self = Self(0x04);
+
+    /// Used by platform firmware to select legacy bios boot options
+    pub const BIOS: Self = Self(0x05);
+
+    /// Represents the end of the device path
+    pub const END: Self = Self(0x7F);
+}
+
+/// [`RawDevicePath`] Sub Types
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(transparent)]
+pub struct RawDevicePathSubType(u8);
+
+impl RawDevicePathSubType {
+    /// Represents a file [Media][`RawDevicePathType::MEDIA`] path
+    pub const MEDIA_FILE: Self = Self(0x04);
+
+    /// Represents the end of the entire [`RawDevicePath`]
+    pub const END_ENTIRE: Self = Self(0xFF);
+
+    /// Represents the end of this [`RawDevicePath`] instance
+    /// and the start of a new one
+    pub const END_INSTANCE: Self = Self(0x01);
+}
+
 /// Raw device path structure
 ///
 /// Device Paths are variable length, unaligned/byte packed structures.
@@ -9,8 +53,8 @@
 #[derive(Debug)]
 #[repr(C, packed)]
 pub struct RawDevicePath {
-    pub ty: u8,
-    pub sub_ty: u8,
+    pub ty: RawDevicePathType,
+    pub sub_ty: RawDevicePathSubType,
     /// Length, including this header
     pub len: [u8; 2],
 }
@@ -23,8 +67,8 @@ impl RawDevicePath {
     /// It is up to you to make sure this is a valid node.
     pub unsafe fn create(ty: u8, sub_ty: u8, len: u16) -> Self {
         Self {
-            ty,
-            sub_ty,
+            ty: RawDevicePathType(ty),
+            sub_ty: RawDevicePathSubType(sub_ty),
             len: len.to_le_bytes(),
         }
     }
@@ -32,8 +76,8 @@ impl RawDevicePath {
     /// Create the end of path node
     pub fn end() -> Self {
         Self {
-            ty: 0x7F,
-            sub_ty: 0xFF,
+            ty: RawDevicePathType::END,
+            sub_ty: RawDevicePathSubType::END_ENTIRE,
             len: 4u16.to_le_bytes(),
         }
     }
